35da30a6ad4711c7672576c0bdd561f775e92a1a
Raman Ivanou <raman_ivanou@epam.com>
2016-05-27 09:56:34 +0300

    add mixin for article

--- src/mixins/hideInactive.js
+++ src/mixins/hideInactive.js
@@ -1,11 +1,12 @@
 export default {
     getInitialState(){
-        return { openedArticle : null}
+        return { openedArticle : null, isOpen :false }
     },  
     openArticle(id) { 
             return  (ev) => {         
                 if (ev) ev.preventDefault();
-                this.setState({ openedArticle: this.state.openedArticle == id ? null : id });
+                this.setState({ openedArticle: this.state.openedArticle == id ? null : id, isOpen : !this.state.isOpen});
+                
             };
     }
 }
--- src/mixins/toggleOpen.js
+++ src/mixins/toggleOpen.js
@@ -1,14 +1,14 @@
 export default {
-    getInitialState() {
-        return {
-            isOpen: false
-        }
-    },
-    toggleOpen(ev) {
-        if (ev) ev.preventDefault()
-        console.log('on click works from mixin')
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+    //getInitialState() {
+        //return {
+            //isOpen: false
+        //}
+    //},
+    //toggleOpen(ev) {
+        //if (ev) ev.preventDefault()
+       // console.log('on click works from mixin')
+       // this.setState({
+            //isOpen: !this.state.isOpen
+      //  })
+   // }
 }
