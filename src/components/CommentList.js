c0ec9abfdc4fbe1128e28f19e500508f516c6617
Roman <kaboom.ua@gmail.com>
2016-06-02 13:46:27 -0700

    Update CommentList.js

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -34,6 +34,7 @@ class CommentList extends Component {
 
     getList() {
         const { isOpen, article} = this.props
+        //у нас есть метод article.getRelation('comments')
        //делать тут обращение к серверу плохой паттерн - render должен быть чистой функцией, к серверу надо обращаться в lifecycle хуках;
         const comments = this.props.comments.filter( (item) => { return article.id === item.article} ).length === 0 && isOpen ?  this.handleGetArticleComments(article.id) :  this.props.comments
         if (!isOpen) return null
